223a9e60f11d9f3ed891d5e5cedbfafacd7ed20d
hossvadi <hossvadi@gmail.com>
2022-07-09 02:08:00 +0200

    LassoRegression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ftplib import error_perm
 from re import X
+from socket import TIPC_DEST_DROPPABLE
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -63,7 +64,67 @@ plt.ylabel('Predicted Prices')
 plt.title('Actual Price vs Predicted Prices')
 plt.show()
 
+# Training the Model
 
 
+# prediction on the training data
+test_data_prediction = lin_reg_model.predict(X_test)
+
+#to compare predicted and original values we should calculate R Square error
+#Sklearn!
+error_score = metrics.r2_score(Y_test, test_data_prediction)
+print("R Squar Error : ", error_score)
+
+#visualize the actual prices and predicted prices
+plt.scatter(Y_test, test_data_prediction)
+plt.xlabel('Actual Prices')
+plt.ylabel('Predicted Prices')
+plt.title('Actual Price vs Predicted Prices')
+plt.show()
+
+
+#########Lasso regression
+
+
+
+#Training the model through LinearRegression
+lass_reg_model = Lasso()
+
+lass_reg_model.fit(X_train, Y_train)
+
+#Model evaluation, predictiong on training data
+lass_training_data_prediction = lass_reg_model.predict(X_train)
+
+#to compare predicted and original values we should calculate R Square error
+#Sklearn!
+error_score = metrics.r2_score(Y_train, lass_training_data_prediction)
+print("R Squar Error : ", error_score)
+
+# Accuracy is 84% = R Squar Error :  0.8427856123435794
+
+#visualize the actual prices and predicted prices
+plt.scatter(Y_train, lass_training_data_prediction)
+plt.xlabel('Actual Prices')
+plt.ylabel('Predicted Prices')
+plt.title('Actual Price vs Predicted Prices')
+plt.show()
+
+# Training the Model
+
+
+# prediction on the training data
+lass_test_data_prediction = lass_reg_model.predict(X_test)
+
+#to compare predicted and original values we should calculate R Square error
+#Sklearn!
+error_score = metrics.r2_score(Y_test, lass_test_data_prediction)
+print("R Squar Error : ", error_score)
+
+#visualize the actual prices and predicted prices
+plt.scatter(Y_test, lass_test_data_prediction)
+plt.xlabel('Actual Prices')
+plt.ylabel('Predicted Prices')
+plt.title('Actual Price vs Predicted Prices')
+plt.show()
 
 
